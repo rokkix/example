b9234fd2f35b58079fe6dee4525d0963f52d6ef2
yury firsanau <rokki123@yandex.ru>
2019-01-31 10:07:32 +0300

    example

--- UnloadingException.php
+++ UnloadingException.php
@@ -2,13 +2,57 @@
 
 namespace App\Exceptions;
 
-use Exception;
-use Throwable;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class UnloadingException extends Exception
+
+class UnauthorizedException extends HttpException
 {
-    public function __construct($message = "", Throwable $previous = null)
+    private $requiredRoles = [];
+
+    private $requiredPermissions = [];
+
+    public static function forRoles(array $roles): self
     {
-        parent::__construct($message, 0, $previous);
+        $message = 'User does not have the right roles.';
+
+        if (config('permission.display_permission_in_exception')) {
+            $permStr = implode(', ', $roles);
+            $message = 'User does not have the right roles. Necessary roles are '.$permStr;
+        }
+
+        $exception = new static(403, $message, null, []);
+        $exception->requiredRoles = $roles;
+
+        return $exception;
+    }
+
+    public static function forPermissions(array $permissions): self
+    {
+        $message = 'User does not have the right permissions.';
+
+        if (config('permission.display_permission_in_exception')) {
+            $permStr = implode(', ', $permissions);
+            $message = 'User does not have the right permissions. Necessary permissions are '.$permStr;
+        }
+
+        $exception = new static(403, $message, null, []);
+        $exception->requiredPermissions = $permissions;
+
+        return $exception;
+    }
+
+    public static function notLoggedIn(): self
+    {
+        return new static(403, 'User is not logged in.', null, []);
+    }
+
+    public function getRequiredRoles(): array
+    {
+        return $this->requiredRoles;
+    }
+
+    public function getRequiredPermissions(): array
+    {
+        return $this->requiredPermissions;
     }
 }
